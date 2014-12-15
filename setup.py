ef1f0b3e8f162aa35c68a90b90c22981e0164faf
mango-mango <mango@rolymobile.com>
2014-12-15 16:50:57 +0800

    mango_lib complete.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mango_lib',
+    version = '1.0',
+    description='This is your awesome module',
+    author='You',
+    author_email='your@email',
+    packages = ['mango_lib'],
+    test_suite = 'your.module.tests',
+)
